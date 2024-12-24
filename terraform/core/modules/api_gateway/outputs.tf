d492b9b106a9b40adf2473a99832e6c73304cdb7
Damien Burks <damien@damienjburks.com>
2024-12-24 13:32:11 -0600

    adding in sqs queue for processing - will add permissions later

--- terraform/core/modules/api_gateway/outputs.tf
+++ terraform/core/modules/api_gateway/outputs.tf
@@ -1,3 +1,3 @@
 output "api_gateway_url" {
-  value = aws_api_gateway_deployment.this.invoke_url
+  value = "${aws_api_gateway_deployment.this.invoke_url}${aws_api_gateway_resource.this.path_part}"
 }
