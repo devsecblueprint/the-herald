eb0d7f466c2fe799135a8ee37a7a6ab52a096379
Damien Burks <damien@damienjburks.com>
2024-12-25 01:13:23 -0600

    commenting out all channels until i fix the issues

--- terraform/core/locals.tf
+++ terraform/core/locals.tf
@@ -1,8 +1,8 @@
 locals {
   YOUTUBE_CHANNEL_HANDLES = [
-    "@damienjburks",
-    "@DayCyberwox",
-    "@damienburks9802"
+    #"@damienjburks",
+    #"@DayCyberwox",
+    #"@damienburks9802"
   ]
   account_id = data.aws_caller_identity.current.account_id
 }
