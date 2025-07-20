8b6789c5b0250b19c1edc04f85ff2cec04d945f4
Damien Burks <damien@damienjburks.com>
2025-07-20 01:44:31 -0500

    run's every hour

--- app/utils/scheduler.py
+++ app/utils/scheduler.py
@@ -34,7 +34,7 @@ class JobScheduler:
             NewsletterService().publish_latest_articles,
             trigger="interval",
             minutes=60,  # Run every hour
-            id="daily_newsletter_job",
+            id="hourly_newsletter_job",
             replace_existing=True,
         )
         self.logger.info("Adding job to publish latest articles every hour.")
